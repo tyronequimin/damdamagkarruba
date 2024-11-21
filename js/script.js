13d7fbacdf80706d149af44456af973e59af059a
tyronequimin <tyronequimin@gmail.com>
2024-11-21 15:48:31 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.nav-links');
+
+    hamburger.addEventListener('click', () => {
+      navLinks.classList.toggle('active');
+    });
+  });
